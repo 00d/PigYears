ff5925d9f0b09aaddfdf861bbb31356839e3bc03
John Britzke <jb@johns-macbook>
2017-08-08 04:01:51 -0700

    Calculator works; image will be fixed

--- Pig Years/ViewController.swift
+++ Pig Years/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearsTextField: UITextField!
+    
+    @IBOutlet weak var finalAgeTextField: UILabel!
+    
+    @IBAction func calcAge(_ sender: Any) {
+        if let years = yearsTextField.text {
+            if let temp = Int(years) {
+                let age = (temp * 4) + 14
+                
+                finalAgeTextField.text = "" + String(age) + " human years!"
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
